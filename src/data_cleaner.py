af0d3a241c6168fe828386964b9885afb0bbf461
Riccardo <riccardo.nicolini@outlook.com>
2024-12-10 16:22:05 +0100

    Choroplath map begin

---
+++ src/data_cleaner.py
@@ -0,0 +1,156 @@
+import pandas as pd
+import numpy as np
+import json
+
+
+# Importa i due dataset dalla cartella dataset
+departures = pd.read_csv('dataset/International_Report_Departures.csv')
+passengers = pd.read_csv('dataset/International_Report_Passengers.csv')
+
+# Visualizza le prime righe dei dataset
+print(departures.head())
+print(passengers.head())
+
+print('Il dataset departures ha', len(departures), 'campioni')
+print('Il dataset passengers ha', len(passengers), 'campioni')
+
+#Verifico se ci sono dei duplicati nei dataset
+print('Il dataset departures ha', len(departures[departures.duplicated()]), 'duplicati')
+print('Il dataset passengers ha', len(passengers[passengers.duplicated()]), 'duplicati')
+
+#Cancello le colonne non utili ai fini dell'analisi o ridondanti
+#date_dte è ridondante con Year e Month
+#usg_apt_id e fg_apt_id sono ridondanti con usg_apt e fg_apt
+#type serve solo per suddividere i due dataset (non utilie perché verranno uniti)
+#carriergroup è un dato che non mi serve
+#Charter e Total sono rimossi perché analizzeremo solo i voli di linea programmati
+#usg_wac e fg_wac sono codici IATA delle zone mondiali, ma le implemento con i codici IATA degli aeroporti
+#airlineid è un dato ridondante con carrier poiché Carrirer è un identificativo univoco per ogni compagnia aerea
+departures = departures.drop(columns=['data_dte', 'usg_apt_id', 'fg_apt_id', 'type', 'carriergroup', 'Total', 'Charter', 'usg_wac', 'fg_wac', 'airlineid'], axis=1)
+passengers = passengers.drop(columns=['data_dte', 'usg_apt_id', 'fg_apt_id', 'type', 'carriergroup', 'Total', 'Charter', 'usg_wac', 'fg_wac', 'airlineid'], axis=1)
+
+# Rinomino le colonne per una migliore comprensione
+departures = departures.rename(columns={'usg_apt_id': 'US_Airport_id','usg_apt': 'US_Airport','fg_apt_id': 'FG_Airport_id', 'fg_apt': 'FG_Airport', 'carrier': 'Airline', 'Scheduled': 'Flights'})
+passengers = passengers.rename(columns={'usg_apt_id': 'US_Airport_id','usg_apt': 'US_Airport','fg_apt_id': 'FG_Airport_id', 'fg_apt': 'FG_Airport', 'carrier': 'Airline', 'Scheduled': 'Passengers'})
+
+
+# Unisce i due dataset in un unico dataset usando come chiave di join le colonne comuni (se non ci sono corrispondenze, non vengono inserite righe)
+#L'inner join è usato per mantenere solo le righe che hanno corrispondenze in entrambi i dataset
+data = pd.merge(departures, passengers, on=['Year', 'Month', 'US_Airport', 'FG_Airport', 'Airline'], how='inner')
+
+
+#Quantifica la differenza tra i dataset iniziali e quello finale (significa che ci sono delle righe che non hanno corrispondenze)
+print('La differenza tra i dataset iniziali e quello finale è di',  len(passengers) - len(data), 'righe')
+
+
+#US_Ariport e FG_Airport sono codici IATA, essi corrisspondono a un aeroporto specifico in un determinato Stato. Voglio creare una colonna con il nome dello Stato
+# Importo il dataset con i codici IATA degli aeroporti
+iata_codes = pd.read_csv('dataset/iata_codes.csv')
+
+#rimuoivo i duplicati dal dataset iata_codes
+iata_codes = iata_codes.drop_duplicates(subset=['iata_code'])
+
+
+#Fai un merge tra data e iata_codes per ottenere il nome degli Stati, usando come chiave di join le colonne US_Airport e FG_Airport
+data = pd.merge(data, iata_codes, left_on='US_Airport', right_on='iata_code', how='inner')
+data = data.rename(columns={'Country_Name': 'US_State', 'Country_Code': 'US_State_id', 'Continent': 'US_Continent'})
+data = data.drop(columns=['iata_code'], axis=1)
+#rimuovi tutti i campioni che hanno us_state_id che non inizia con 'US-'
+data = data.drop(data[~data['US_State_id'].str.startswith('US-')].index)
+
+data = pd.merge(data, iata_codes, left_on='FG_Airport', right_on='iata_code', how='inner')
+data = data.rename(columns={'Country_Name': 'FG_State', 'Country_Code': 'FG_State_id', 'Continent': 'FG_Continent'})
+data = data.drop(columns=['iata_code'], axis=1)
+
+
+#Riordino le colonne nell'ordine che preferisco
+column_order = [
+    'Year', 'Month', 'US_Airport', 'US_State', 'US_State_id', 'US_Continent', 
+    'FG_Airport', 'FG_State', 'FG_State_id', 'FG_Continent', 'Flights', 'Passengers'
+]
+data = data[column_order]
+
+
+#Ora voglio fare in modo che le tratte rispetto agli aeroporti siano uniche, quindi raggruppo per compagnia aerea sommando i voli e i passeggeri per poi cancellare la colonna Airline
+data = data.drop(columns=['US_Airport', 'FG_Airport'])
+data = data.groupby(['Year','Month', 'US_State' ,'US_State_id', 'US_Continent', 'FG_State', 'FG_State_id', 'FG_Continent'], as_index=False).agg({
+    'Passengers': 'sum',
+    'Flights': 'sum'
+})
+
+
+#drop dei campioni che hanno meno di un tot di passeggeri o voli
+data = data.drop(data[(data['Passengers'] == 0) | (data['Flights'] == 0)].index)
+
+#conta gli stati US_state_id unici
+print('Ci sono', len(data['US_State_id'].unique()), 'stati negli Stati Uniti')
+
+#stampa il numero di passeggeri minimo e massimo di tutti i collegamenti
+print('Il numero minimo di passeggeri è', data['Passengers'].min())
+print('Il numero massimo di passeggeri è', data['Passengers'].max())
+#stampa il numero di voli minimo e massimo di tutti i collegamenti
+print('Il numero minimo di voli è', data['Flights'].min())
+print('Il numero massimo di voli è', data['Flights'].max())
+
+#Verifica del dataset finale (head, lunghezza e valori nulli)
+print(data.head())
+
+print('Il dataset finale ha', len(data), 'campioni')
+
+print(data.isnull().sum())
+
+#Salva il dataset finale in un file csv
+data.to_csv('dataset/International_Report.csv', index=False)
+
+
+###############################################################  JSON  ###############################################################
+
+#Voglio creare un file json suddiviso in nodi (che erappresentano gli Stati) e archi (che rappresentano le tratte)
+#Gli stati avranno come attributi il nome, il codice ISO e il continente
+#Le tratte avranno come attributi il numero di voli e passeggeri, l'aeroporto di partenza e di arrivo e l'anno e mese
+
+#Noti (stati)
+nodes = []
+for state in pd.concat([data['US_State'], data['FG_State']]).unique():
+    # Per trovare il codice ISO e il continente, dobbiamo verificare prima se lo stato è negli Stati Uniti o in un altro paese
+    if state in data['US_State'].values:
+        # Se lo stato è negli Stati Uniti, prendi il codice ISO da US_State_id e il continente da US_Continent
+        iso_code = data[data['US_State'] == state]['US_State_id'].values[0]
+        continent = data[data['US_State'] == state]['US_Continent'].values[0]
+    elif state in data['FG_State'].values:
+        # Se lo stato è in un altro paese, prendi il codice ISO da FG_State_id e il continente da FG_Continent
+        iso_code = data[data['FG_State'] == state]['FG_State_id'].values[0]
+        continent = data[data['FG_State'] == state]['FG_Continent'].values[0]
+    else:
+        # Se non trovi lo stato, salta
+        continue
+    
+    # Aggiungi il nodo con il nome, il codice ISO e il continente
+    nodes.append({
+        'name': state,
+        'iso_code': iso_code,
+        'continent': continent
+    })
+
+# Arichi (tratte)
+edges = []
+for _, row in data.iterrows():
+  edges.append({
+    'US_state': row['US_State'],
+    'FG_state': row['FG_State'],
+    'flights': row['Flights'],
+    'passengers': row['Passengers'],
+    'year': row['Year'],
+    'month': row['Month']
+  })
+
+dataset = {
+  'nodes': nodes,
+  'edges': edges
+}
+
+print(len(dataset['nodes']), len(dataset['edges']))
+
+with open('dataset/International_Report.json', 'w') as f:
+  json.dump(dataset, f, indent=3)
+
---
+++ src/script.js
@@ -0,0 +1,181 @@
+// SVG and dimensions
+var svg = d3.select("svg"),
+    width = window.innerWidth,
+    height = window.innerHeight;
+
+svg.attr("width", width)
+   .attr("height", height);
+
+// Map and projection (americocentric, without cuts)
+var projection = d3.geoMercator()
+    .scale(width / 6)
+    .center([-98, 38])
+    .rotate([100, 0]) // Rotazione per centrare l'America
+    .translate([width / 2, height / 2]);
+
+var path = d3.geoPath().projection(projection);
+
+// Data and color scale
+var data = d3.map();
+var colorScale = d3.scaleThreshold()
+  .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
+  .range(d3.schemeBlues[7]);
+
+// Create groups for the map layers
+var worldGroup = svg.append("g").attr("class", "world");
+var statesGroup = svg.append("g").attr("class", "us-states");
+
+// Load external data and boot
+d3.queue()
+  .defer(d3.json, "../dataset/world-states.geojson.json") // Mappa mondiale
+  .defer(d3.json, "../dataset/us-states.geojson.json") // Mappa degli stati USA
+  .defer(d3.csv, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv", function(d) { data.set(d.code, +d.pop); }) // Dati mondiali
+  .await(ready);
+
+function ready(error, world, usStates) {
+  if (error) throw error;
+
+function zoomToBoundingBox(group, bbox, scaleMultiplier = 1) {
+    const [[x0, y0], [x1, y1]] = bbox;
+    const widthBBox = x1 - x0;
+    const heightBBox = y1 - y0;
+
+    const scale = Math.min(width / widthBBox, height / heightBBox) * scaleMultiplier;
+    const translate = [(width - scale * (x0 + x1)) / 2, (height - scale * (y0 + y1)) / 2];
+
+    svg.transition()
+    .duration(750)
+    .call(zoom_handler.transform, d3.zoomIdentity.translate(translate[0], translate[1]).scale(scale));
+}
+
+//[581.88 256.4319][972.2 528.75]
+
+const usaBBox = [[900, 356], [2100, 500]];
+zoomToBoundingBox(statesGroup, usaBBox, 1.5);
+
+// Draw the world map
+worldGroup.selectAll("path")
+.data(world.features.filter(function(d) {
+// Esclude gli Stati Uniti (supponendo che l'ID sia 'USA')
+return d.id !== "USA";
+}))
+.enter()
+.append("path")
+.attr("d", path)
+.attr("fill", function(d) {
+d.total = data.get(d.id) || 0; //Colora in base alla popolazione
+return colorScale(d.total);
+})
+.style("stroke", "transparent")
+.attr("class", "Country")
+.style("opacity", .8)
+.on("mouseover", function(event, d) {
+d3.select(this)
+    .transition()
+    .duration(200)
+    .style("stroke-width", 1)
+    .style("opacity", 1)
+    .style("stroke", "black");
+})
+.on("mouseleave", function(event, d) {
+d3.select(this)
+    .transition()
+    .duration(200)
+    .style("stroke-width", 1)
+    .style("opacity", .8)
+    .style("stroke", "transparent");
+});
+
+// Draw the US states map
+statesGroup.selectAll("path")
+.data(usStates.features)
+.enter()
+.append("path")
+.attr("d", path)
+.attr("fill", function(d) {
+d.total = data.get(d.properties.NAME) || 0; //TO-DO: Colora in base ai collegamenti
+return colorScale(d.total);
+})
+.style("stroke", "transparent")
+.attr("class", "State")
+.style("opacity", .8)
+.on("mouseover", function(event, d) {
+d3.select(this)
+    .transition()
+    .duration(200)
+    .style("stroke-width", 0.5)
+    .style("opacity", 1)
+    .style("stroke", "black");
+})
+.on("mouseleave", function(event, d) {
+d3.select(this)
+    .transition()
+    .duration(200)
+    .style("stroke-width", 0.5)
+    .style("opacity", .8)
+    .style("stroke", "transparent");
+})
+.on("click", function(event, d) {
+    const selectedState = d3.select(this);
+
+    // Verifica se lo stato è già evidenziato
+    const currentFill = selectedState.style("fill");
+
+    if (currentFill === "red") {
+        // Se è evidenziato, ripristina il colore originale
+        selectedState.style("fill", function(d) {
+            d.total = data.get(d.properties.NAME) || 0;
+            return colorScale(d.total);
+        });
+
+        // Verifica se non ci sono più stati rossi
+        const anyRedState = statesGroup.selectAll(".State").filter(function() {
+            return d3.select(this).style("fill") === "red";
+        }).empty();
+
+        // Se non ci sono più stati rossi, zooma sugli Stati Uniti
+        if (anyRedState) {
+            zoomToBoundingBox(worldGroup, usaBBox, 1);
+        }
+    } else {
+        // Se non è evidenziato, colora di rosso
+        selectedState.style("fill", "red");
+        const worldBBox = [[550, 0], [2500, 1000]];
+        zoomToBoundingBox(worldGroup, worldBBox, 1);
+        console.log(worldBBox);
+
+        // Se non ci sono stati rossi prima, esegui zoomToBoundingBox sugli USA
+        const anyRedState = statesGroup.selectAll(".State").filter(function() {
+            return d3.select(this).style("fill") === "red";
+        }).empty();
+
+        if (anyRedState) {
+            zoomToBoundingBox(worldGroup, usaBBox, 1);
+        }
+    }
+});
+
+
+
+}
+
+// Zoom handling
+var zoom_handler = d3.zoom()
+    .scaleExtent([1, 8])
+    .on("zoom", zoom_actions);
+
+zoom_handler(svg);
+
+function zoom_actions() {
+    worldGroup.attr("transform", d3.event.transform);
+    statesGroup.attr("transform", d3.event.transform);
+}
+// Resize handling
+window.addEventListener("resize", () => {
+    width = window.innerWidth;
+    height = window.innerHeight;
+    svg.attr("width", width).attr("height", height);
+    projection.translate([width / 2, height / 2]).scale(width / 6);
+    worldGroup.selectAll("path").attr("d", path);
+    statesGroup.selectAll("path").attr("d", path);
+});
